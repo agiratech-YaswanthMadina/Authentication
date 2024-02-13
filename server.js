bb9d6c1deef76e07360822ef6f705ab78b231e33
agiratech-YaswanthMadina <yaswanth7888@gmail.com>
2024-02-13 16:21:02 +0530

    code

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const bcrypt = require('bcryptjs')
+app.set('view-engine', 'ejs')
+
+const users = []
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', {name: 'yaswanth'})
+})
+
+app.use(express.urlencoded({extended: false}))
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.get('/register', async(req, res) => {
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword,
+        })
+        res.redirect('/login')
+    }catch{
+        res.redirect('/register')
+    }
+})
+
+app.post('/login', (req, res) => {
+    
+})
+
+app.post('/register', (req, res) => {
+
+})
+
+app.listen(3000)
